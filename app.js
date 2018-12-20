d50c5dceedceee4b2aa9246003c4a4c42201fcd8
Daniel-Pfeffer <34685064+daniel-pfeffer@users.noreply.github.com>
2018-12-20 08:29:06 +0100

    Server may be finished

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
+var Person = require('./person.js');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
+let personArray = [];
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.listen(8080, () => {
-    console.log('Server started')
+    console.log('Server started');
+    fileRead();
 });
 
 
 app.route('/tdot/getAll').get((req, res) => {
     res.send({
-        persons: [{
-            name: 'gunther',
-            mail: 'ex.e@at.com',
-            guess: 1027
-        }, {
-            name: 'daniel',
-            mail: 'ex.e@daniel.com',
-            guess: 2090
-        }]
+        personArray: personArray
     });
 });
 
-//app.use(bodyParser.json);
 
 app.route('/tdot/addGuess').put((req, res) => {
     let body = '';
@@ -31,12 +27,51 @@ app.route('/tdot/addGuess').put((req, res) => {
         body += chunk.toString();
     });
     req.on('end', () => {
-        console.log(body);
-        res.end('ok')
+        let data = JSON.parse(body);
+        if (!findPerson(data)) {
+            personArray.push(new Person(data['name'], data['mail'], data['guess']));
+            fileWrite();
+            res.end('ok')
+        } else {
+            res.end('wot m8');
+        }
     });
 });
 
 app.route('/tdot/getPerson').post((req, res) => {
-
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+        let data = JSON.parse(body);
+        if(findPerson(data)){
+            res.status(418).send("lel")
+        } else {
+            res.status(200).send("ok")
+        }
+    });
 });
 
+function fileRead() {
+    let data = fs.readFileSync('persons.json');
+    let persons = JSON.parse(data);
+    for (let personsKey in persons) {
+        personArray.push(new Person(persons[personsKey]['name'], persons[personsKey]['mail'], persons[personsKey]['guess']));
+    }
+}
+
+function fileWrite() {
+    let data = JSON.stringify(personArray);
+    fs.writeFile('persons.json', data)
+}
+
+function findPerson(data) {
+    const mail = data['mail'];
+    for (let personArrayElement of personArray) {
+        if(personArrayElement.mail === mail){
+            return true
+        }
+    }
+    return false;
+}
---
+++ person.js
@@ -0,0 +1,16 @@
+/**
+ * Created by PhpStorm.
+ * User: danielpfeffer
+ * Date: 2018-12-20
+ * Time: 07:24
+ */
+'use strict';
+
+module.exports = class Person {
+
+    constructor(name, mail, guess) {
+        this.name = name;
+        this.mail = mail;
+        this.guess = guess;
+    }
+};
