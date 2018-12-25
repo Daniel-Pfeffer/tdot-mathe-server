ef5b8a3b56fbb1be327e385c5ec79816b4eb7faa
danielpfeffer <daniel.pfeffer@breathless-pictures.at>
2018-12-25 10:59:24 +0100

    Server finished  added Acces-Control-Allow   to allow server functions without cors  added logs when a route is called

--- app.js
+++ app.js
@@ -10,15 +10,35 @@ app.listen(8080, () => {
     fileRead();
 });
 
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 app.route('/tdot/getAll').get((req, res) => {
+    console.log("getAll called");
     res.send({
         personArray: personArray
     });
 
 });
 
-app.route('/tdot/addGuess').put((req, res) => {
+app.route('/tdot/addGuess').post((req, res) => {
+    console.log("addGuess called");
     let body = '';
     req.on('data', chunk => {
         body += chunk.toString();
@@ -36,6 +56,7 @@ app.route('/tdot/addGuess').put((req, res) => {
 });
 
 app.route('/tdot/getPerson').post((req, res) => {
+    console.log("getPerson called");
     let body = '';
     req.on('data', chunk => {
         body += chunk.toString();
